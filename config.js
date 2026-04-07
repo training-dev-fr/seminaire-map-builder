0e32f6320492df4d740e58deb5a0847eea61bd84
training-dev <contact@training-dev.fr>
2026-04-07 16:45:47 +0200

    tile + config

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    "width": 10,
+    "height": 10,
+    "canvasWidth":500,
+    "canvasHeight":500
+}
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+import config from "./config.js";
+
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext("2d");
 
-context.beginPath();
-context.arc(250,250,50,0,Math.PI/2);
-context.fillStyle = "#1565c0";
-context.strokeStyle = "rgb(255,0,0)";
-context.lineWidth = 5;
-context.fill();
-context.stroke();
+context.fillStyle= "#ff0000";
+context.fillRect(0,0,500,500);
+
+async function loadImage(){
+    const result = await fetch('./square.png');
+    const image = await createImageBitmap(await result.blob());
+    const squareWidth = (config.canvasWidth / config.width) - 1;
+    const squareHeight = (config.canvasHeight / config.height) - 1;
+    for(let x=0; x<= config.width -1;x++){
+        for(let y=0; y<= config.height-1;y++){
+            context.drawImage(image, (x * squareWidth) + x,(y * squareHeight) +y,squareWidth,squareHeight);
+        }
+    }
+    
+}
+
+loadImage();

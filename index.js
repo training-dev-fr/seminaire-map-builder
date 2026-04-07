96eebe5bcecd8ce170c376036ac1cf2c998cbaee
training-dev <contact@training-dev.fr>
2026-04-07 15:17:00 +0200

    circle

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const canvas = document.querySelector('canvas');
 const context = canvas.getContext("2d");
 
 context.beginPath();
-context.moveTo(75,50);
-context.lineTo(100,75);
-context.lineTo(100,25);
-context.closePath();
-context.fillStyle = "rgb(0,128,255)";
-context.fill();
+context.arc(250,250,50,0,Math.PI/2);
+context.fillStyle = "#1565c0";
 context.strokeStyle = "rgb(255,0,0)";
 context.lineWidth = 5;
+context.fill();
 context.stroke();
